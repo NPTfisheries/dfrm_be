11be6317d082ddd09442c0a6071da6ec1a12f9d3
Ryan <ryank@nezperce.org>
2023-08-04 08:23:35 -0600

    fixed function for superuser

--- website/account/models.py
+++ website/account/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.contrib.auth.password_validation import validate_password
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from PIL import Image
@@ -18,26 +19,24 @@ class UserManager(BaseUserManager):
         if not last_name:
             raise ValueError('Last name is required')
         
-        self.validate_password(password)
+        validate_password(password)
 
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, password, **extra_fields):
+    def create_superuser(self, email, first_name, last_name, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
         extra_fields.setdefault('is_active', True)
-
-        self.validate_password(password)
 
         if extra_fields.get('is_staff') is not True:
             raise ValueError('Superuser must have is_staff = True')
         if extra_fields.get('is_superuser') is not True:
             raise ValueError('Superuser must have is_superuser = True')
 
-        return self.create_user(email, password, **extra_fields)
+        return self.create_user(email, first_name, last_name, password, **extra_fields)
     
 class User(AbstractUser):
 
