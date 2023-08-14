2c5bcf7a5b7a97d984e2ca3183aac46a9489c42c
Ryan <ryank@nezperce.org>
2023-08-14 16:39:24 -0600

    working with auto group perms

--- website/common/initial_data.py
+++ website/common/initial_data.py
@@ -1,86 +1,111 @@
-from django.contrib.auth.management import create_permissions
+#from django.contrib.auth.management import create_permissions
 from django.contrib.auth.models import Group, Permission
-
-# def populate_groups(apps, schema_editor):
-
-# # Create user groups
-#     user_roles = ["Admin", "Manager", "Professional", "Technician", "Guest"]
-#     for name in user_roles:
-#         Group.objects.create(name=name)
-
-#     # Permissions have to be created before applying them
-#     for app_config in apps.get_app_configs():
-#         app_config.models_module = True
-#         create_permissions(app_config, verbosity=0)
-#         app_config.models_module = None
-
-#     # Assign model-level permissions to Admin 
-#     all_perms = Permission.objects.all()
-#     admin_perms = [i for i in all_perms if i.content_type.app_label == "administration"]
-#     Group.objects.get(name="Admin").permissions.add(*admin_perms)
+from django.contrib.contenttypes.models import ContentType
 
 def populate_groups(apps, schema_editor):
-    Group = apps.get_model('auth', 'Group')
-    Permission = apps.get_model('auth', 'Permission')
+
+    user_roles = ["Admin", "Manager", "Professional", "Technician", "Guest"]
+    for role_name in user_roles:
+        group, created = Group.objects.get_or_create(name=role_name)
+
 
     # Define role-specific permissions by app and model
     role_permissions = {
-        "Admin": {
-            "account.User": ["view", "add", "change", "delete"],
-            "account.Profile": ["view", "add", "change", "delete"],      
-            "administration.Department": ["view", "add", "change", "delete"],
-            "administration.Division": ["view", "add", "change", "delete"],
-            "administration.Project": ["view", "add", "change", "delete"],
-            "administration.Subproject": ["view", "add", "change", "delete"],
-            "administration.Task": ["view", "add", "change", "delete"],
-            "files.Image": ["view", "add", "change", "delete"],
+        "Admin":{
+            'account':{
+                'user':["view", "add", "change", "delete"],
+                'profile':["view", "add", "change", "delete"]
+            },
+            'administration':{
+                'department':["view","add","change","delete"],
+                'division':["view","add","change","delete"],
+                'project':["view","add","change","delete"],
+                'subproject':["view","add","change","delete"],
+                'task':["view","add","change","delete"],
+            },
+            'files':{
+                'image':["view","add","change","delete"]
+            }
         },
-        "Manager": {
-            "account.User": ["view", "change"],
-            "account.Profile": ["view", "change"],      
-            "administration.Department": ["view", "add", "change"],
-            "administration.Division": ["view", "add", "change"],
-            "administration.Project": ["view", "add", "change"],
-            "administration.Subproject": ["view", "add", "change"],
-            "administration.Task": ["view", "add", "change"],
-            "files.Image": ["view", "add", "change"],
+        "Manager":{
+            'account':{
+                'user':["view", "change"],
+                'profile':["view", "change"]
+            },
+            'administration':{
+                'department':["view","add","change"],
+                'division':["view","add","change"],
+                'project':["view","add","change"],
+                'subproject':["view","add","change"],
+                'task':["view","add","change"],
+            },
+            'files':{
+                'image':["view","add","change"]
+            }
         },
-        "Professional": {
-            "account.User": ["view", "change"],
-            "account.Profile": ["view", "change"],  
-            "administration.Department": ["view"],
-            "administration.Division": ["view"],
-            "administration.Project": ["view", "add", "change"],
-            "administration.Subproject": ["view", "add", "change"],
-            "administration.Task": ["view", "add", "change"],
-            "files.Image": ["view", "add", "change"],
+        "Professional":{
+            'account':{
+                'user':["view","change"],
+                'profile':["view", "change"]
+            },
+            'administration':{
+                'department':["view"],
+                'division':["view"],
+                'project':["view","add","change"],
+                'subproject':["view","add","change"],
+                'task':["view","add","change"],
+            },
+            'files':{
+                'image':["view"]
+            }
         },
-        "Technician": {
-            "account.User": ["view", "change"],
-            "account.Profile": ["view", "change"], 
-            "administration.Department": ["view"],
-            "administration.Division": ["view"],
-            "administration.Project": ["view"],
-            "administration.Subproject": ["view"],
-            "administration.Task": ["view"],
-            "files.Image": ["view"],
+        "Technician":{
+            'account':{
+                'user':["view", "change"],
+                'profile':["view", "change"]
+            },
+            'administration':{
+                'department':["view"],
+                'division':["view"],
+                'project':["view"],
+                'subproject':["view"],
+                'task':["view"],
+            },
+            'files':{
+                'image':["view"]
+            }
         },
-        "Guest":  {
-            "account.User": ["view"],
-            "account.Profile": ["view"],
-            "administration.Department": ["view"],
-            "administration.Division": ["view"],
-            "administration.Project": ["view"],
-            "administration.Subproject": ["view"],
-            "administration.Task": ["view"],
-            "files.Image": ["view"],
-        }
+        "Guest":{
+            'account':{
+                'user':["view"],
+                'profile':["view"]
+            },
+            'administration':{
+                'department':["view"],
+                'division':["view"],
+                'project':["view"],
+                'subproject':["view"],
+                'task':["view"],
+            },
+            'files':{
+                'image':["view"]
+            }
+        }  
     }
 
-    for role_name, model_permissions in role_permissions.items():
-        group, created = Group.objects.get_or_create(name=role_name)
-
-        for model, permissions in model_permissions.items():
-            for codename in permissions:
-                permission = Permission.objects.get(codename=codename, content_type__model=model.lower())
-                group.permissions.add(permission)
+    for role_name, app in role_permissions.items():
+        print(f"Role name: {role_name}")
+        group = Group.objects.get(name = role_name)
+        print(f"Group is: {group}")
+        for app_label, model in app.items():
+            print(f"App label: {app_label}")
+            for model_name, permissions in model.items():
+                print(f"Model name: {model_name}")
+                content_type = ContentType.objects.get(app_label = app_label, model=model_name) #takes app_label and model
+                print(f"Permissions: {permissions}")
+                for permission in permissions:
+                    print(f"Permission: {permission}")
+                    codename = f"{permission}_{model_name}"
+                    print(f"Codename is: {codename}")
+                    permission = Permission.objects.get(codename=codename, content_type=content_type) #codename = 'view_user' works
+                    group.permissions.add(permission)
