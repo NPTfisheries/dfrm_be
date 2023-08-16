2226116d02bfc42b099d67b479c75b8aa7d578ca
Ryan <ryank@nezperce.org>
2023-08-16 12:54:41 -0600

    fixed image get requests and serializers to include user information instead of primary key only

--- website/administration/serializers.py
+++ website/administration/serializers.py
@@ -1,38 +1,58 @@
 #administration/serializers.py
 from rest_framework import serializers
 from common.serializers import BaseModelSerializer
+from account.serializers import UserSerializer
 from account.models import User
 from administration.models import Department, Division, Project, Subproject, Task
 
-class DepartmentSerializer(BaseModelSerializer):
+class BaseAdminSerializer(BaseModelSerializer):
+
+    def to_representation(self, instance):
+        # Override the 'user' field representation for GET requests
+        if self.context['request'].method == 'GET':
+            return {
+                **super().to_representation(instance),
+                'manager': UserSerializer(instance.manager).data,
+                'deputy': UserSerializer(instance.deputy).data,
+                'assistant': UserSerializer(instance.assistant).data,
+                'staff': UserSerializer(instance.staff.all(), many=True).data
+            }
+        return super().to_representation(instance)
+
+    def create(self, validated_data):
+        ids = validated_data.pop('staff')
+        instance = super().create(validated_data)
+        instance.staff.set(ids)
+        instance.save()
+        return instance  
+
+
+class DepartmentSerializer(BaseAdminSerializer):
     class Meta:
         model = Department
         fields = ['id', 'slug', 'name', 'description', 'manager', 'deputy', 'assistant', 'staff', 'img_banner', 'img_card']
         
-    def create(self, validated_data):
-        ids = validated_data.pop('staff')
-        instance = super().create(validated_data)
-        instance.staff.set(ids)
-        return instance   
 
-class DivisionSerializer(BaseModelSerializer):
-    #department = DepartmentSerializer() # creates a nested JSON object where department is an object with all department fields.
-    # leaving the above line commented out will only allow POST body to only have pk, and get requests will only have department pk
+class DivisionSerializer(BaseAdminSerializer):
     class Meta:
         model = Division
         fields = ['id', 'department', 'slug', 'name', 'description', 'manager', 'deputy', 'assistant', 'staff', 'img_banner', 'img_card']
 
-    def create(self, validated_data):
-        ids = validated_data.pop('staff')
-        instance = super().create(validated_data)
-        instance.staff.set(ids)
-        return instance  
 
 class ProjectSerializer(BaseModelSerializer):
     project_leader = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(), many=True)
     class Meta:
         model = Project
         fields = ['id', 'slug', 'name', 'description', 'project_leader', 'img_banner', 'img_card']
+
+    def to_representation(self, instance):
+        # Override the 'user' field representation for GET requests
+        if self.context['request'].method == 'GET':
+            return {
+                **super().to_representation(instance),
+                'project_leader': UserSerializer(instance.project_leader.all(), many=True).data
+            }
+        return super().to_representation(instance)
 
     def create(self, validated_data):
         ids = validated_data.pop('project_leader')
@@ -43,9 +63,27 @@ class ProjectSerializer(BaseModelSerializer):
 class SubprojectSerializer(BaseModelSerializer):
     class Meta:
         model = Subproject
-        fields = ['id', 'project', 'slug', 'name', 'description', 'lead', 'img_banner', 'img_card']  
+        fields = ['id', 'project', 'slug', 'name', 'description', 'lead', 'img_banner', 'img_card']
+
+    def to_representation(self, instance):
+        # Override the 'user' field representation for GET requests
+        if self.context['request'].method == 'GET':
+            return {
+                **super().to_representation(instance),
+                'lead': UserSerializer(instance.lead).data
+            }
+        return super().to_representation(instance)  
 
 class TaskSerializer(BaseModelSerializer):
     class Meta:
         model = Task
-        fields = ['id', 'subproject', 'slug', 'name', 'description', 'supervisor', 'img_banner', 'img_card'] 
+        fields = ['id', 'subproject', 'slug', 'name', 'description', 'supervisor', 'img_banner', 'img_card']
+
+    def to_representation(self, instance):
+        # Override the 'user' field representation for GET requests
+        if self.context['request'].method == 'GET':
+            return {
+                **super().to_representation(instance),
+                'supervisor': UserSerializer(instance.supervisor).data
+            }
+        return super().to_representation(instance)   
