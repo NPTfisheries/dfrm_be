7db0a8506a99d9227e9f677179164053a63eb5c8
Ryan <ryank@nezperce.org>
2023-09-13 11:08:01 -0600

    initial migrations reset

--- website/common/migrations/populate_groups.py
+++ website/common/migrations/populate_groups.py
@@ -7,11 +7,7 @@ from ..initial_data import populate_groups
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('account', '0001_initial'),
-        ('administration', '0001_initial'),
-        ('files', '0001_initial'),
         ('sessions', '0001_initial'),
-        ('common', '0001_initial'),
     ]
 
     operations = [
