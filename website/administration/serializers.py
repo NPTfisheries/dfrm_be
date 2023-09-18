df7379fc48291c635ed71bfc2c06f6c4bb318e6b
Tyler Stright <tylers@nezperce.org>
2023-09-18 08:49:17 -0600

    removed two commas creating nested array returns we don't want

--- website/administration/serializers.py
+++ website/administration/serializers.py
@@ -158,8 +158,8 @@ class FacilitySerializer(BaseModelSerializer, GeoFeatureModelSerializer):
             representation['properties']['manager']= UserSerializer(instance.manager).data
             representation['properties']['deputy']= UserSerializer(instance.deputy).data
             representation['properties']['assistant']= UserSerializer(instance.assistant).data
-            representation['properties']['staff']= UserSerializer(instance.staff.all(), many=True).data,
-            representation['properties']['img_banner']= ImageSerializer(instance.img_banner).data,
+            representation['properties']['staff']= UserSerializer(instance.staff.all(), many=True).data
+            representation['properties']['img_banner']= ImageSerializer(instance.img_banner).data
             representation['properties']['img_card']= ImageSerializer(instance.img_card).data
             return representation
         return super().to_representation(instance)
