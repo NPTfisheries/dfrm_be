d53cb84d001e448d60c553f0faeb8eaec42f6a93
Tyler Stright <tylers@nezperce.org>
2023-09-28 15:18:01 -0600

    added django server IP to allowed_hosts

--- website/settings.py
+++ website/settings.py
@@ -32,7 +32,9 @@ SECRET_KEY=env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1:8000'
+]
 
 
 # Application definition
