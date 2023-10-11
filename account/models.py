9759e9e26e465329ae39f66ad539c581a3329565
Tyler Stright <tylers@nezperce.org>
2023-10-11 16:11:00 -0600

    updated profile img hxw

--- account/models.py
+++ account/models.py
@@ -62,7 +62,7 @@ class User(AbstractUser):
     #class_description = models.PositiveSmallIntegerField(choices=CLASS_CHOICES)
 
     class Meta:
-        ordering = ['first_name', 'last_name']
+        ordering = ['last_name', 'first_name']
     
     objects = UserManager()
     
@@ -98,7 +98,7 @@ class Profile(models.Model):
         super().save(*args, **kwargs)
         img = Image.open(self.photo.path)
 
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
+        if img.height > 500 or img.width > 500:
+            output_size = (500, 500)
             img.thumbnail(output_size)
             img.save(self.photo.path)
