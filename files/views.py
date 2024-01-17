6fd37991557f40a6fae4ed7baf5c10b87583d5ef
Tyler Stright <tylers@nezperce.org>
2024-01-17 15:31:15 -0700

    updated delete methods for files to remove the file from the folder and not just the database

--- files/views.py
+++ files/views.py
@@ -1,4 +1,6 @@
-from rest_framework import viewsets
+import os 
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 from files.models import Image, Document
 from files.serializers import ImageSerializer, DocumentSerializer
 from common.views import CustomObjectPermissions
@@ -9,7 +11,37 @@ class ImageViewSet(viewsets.ModelViewSet):
     serializer_class = ImageSerializer
     permission_classes = [CustomObjectPermissions]
 
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+
+        # Get the file path from the instance
+        file_path = instance.image.path
+
+        # Delete the file from the storage
+        if os.path.exists(file_path):
+            os.remove(file_path)
+
+        # Delete the database record
+        self.perform_destroy(instance)
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 class DocumentViewSet(viewsets.ModelViewSet):
     queryset = Document.objects.all()
     serializer_class = DocumentSerializer
     permission_classes = [CustomObjectPermissions]
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+
+        # Get the file path from the instance
+        file_path = instance.document.path
+
+        # Delete the file from the storage
+        if os.path.exists(file_path):
+            os.remove(file_path)
+
+        # Delete the database record
+        self.perform_destroy(instance)
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
