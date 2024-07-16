7a10a39d0287169b45c2b5379b0952a34c6a817c
Tyler Stright <tylers@nezperce.org>
2024-07-16 08:51:46 -0600

    activated prints for testing

--- common/utils.py
+++ common/utils.py
@@ -7,11 +7,11 @@ def resize_image(instance, image_field, min_width, min_height):
     if getattr(instance, image_field):
         img_path = getattr(instance, image_field).path
         img = Image.open(img_path)
-        # print('common/utils/image_processing: Image successfully opened...', flush=True)
+        print('common/utils/image_processing: Image successfully opened...', flush=True)
 
         # if one of the dimensions matches, it doesn't need a resize.
         if not (round(img.size[0], 0) == round(min_width, 0) or round(img.size[1], 0) == round(min_height, 0)):
-            # print('common/utils/image_processing: Resizing image.', flush=True)
+            print('common/utils/image_processing: Resizing image.', flush=True)
             # print('common/utils/image_processing: First load size:', os.path.getsize(img_path), flush=True)
             
             # cannot save an alpha to jpg, so we need to convert .png to rgb
@@ -32,7 +32,7 @@ def resize_image(instance, image_field, min_width, min_height):
             
             # save resized
             img.resize(new_dims, Image.Resampling.LANCZOS).save(img_path)
-            # print('common/utils/image_processing: Saved to', img_path)
+            print('common/utils/image_processing: Saved to', img_path)
             # print('common/utils/image_processing: Final Size:', os.path.getsize(img_path), flush=True)
         else:
             print('common/utils/image_processing: Image is proper size - skipping resize.', flush=True)
