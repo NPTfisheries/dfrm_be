20cb80fbc81fcce98ea317f103b0510a8d1f1225
Tyler Stright <tylers@nezperce.org>
2024-07-16 08:36:00 -0600

    commented destroy method for image

--- files/views.py
+++ files/views.py
@@ -11,22 +11,22 @@ class ImageViewSet(viewsets.ModelViewSet):
     serializer_class = ImageSerializer
     permission_classes = [CustomObjectPermissions]
 
-    def destroy(self, request, *args, **kwargs):
-        instance = self.get_object()
-        print('DESTROY IMAGE!', flush=True)
+    # def destroy(self, request, *args, **kwargs):
+    #     instance = self.get_object()
+    #     print('DESTROY IMAGE!', flush=True)
 
-        # Get the file path from the instance
-        file_path = instance.image.path
-        print('Image Key:', file_path, flush=True)
+    #     # Get the file path from the instance
+    #     file_path = instance.image.path
+    #     print('Image Key:', file_path, flush=True)
 
-        # Delete the file from local storage (dev)
-        if os.path.exists(file_path):
-            os.remove(file_path)
+    #     # Delete the file from local storage (dev)
+    #     if os.path.exists(file_path):
+    #         os.remove(file_path)
 
-        # Delete the database record
-        self.perform_destroy(instance)
+    #     # Delete the database record
+    #     self.perform_destroy(instance)
 
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    #     return Response(status=status.HTTP_204_NO_CONTENT)
     
 class DocumentViewSet(viewsets.ModelViewSet):
     queryset = Document.objects.all()
