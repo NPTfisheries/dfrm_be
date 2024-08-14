e2900bca37fe73f7218824c5dab35b4c76a7f9c4
Tyler Stright <tylers@nezperce.org>
2024-08-14 11:33:33 -0600

    hard coded aws region

--- common/utils.py
+++ common/utils.py
@@ -57,7 +57,7 @@ def delete_s3_object(object_key):
         's3',
         aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
         aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
-        # region_name=settings.AWS_S3_REGION_NAME,
+        region_name='us-west-2',
     )
     try:
         s3.delete_object(Bucket=settings.AWS_STORAGE_BUCKET_NAME, Key=object_key)
