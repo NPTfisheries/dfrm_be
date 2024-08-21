89b8e146ded0d4206405505d14134bdd096b00ab
Tyler Stright <tylers@nezperce.org>
2024-08-21 10:23:41 -0600

    blah

--- website/settings.py
+++ website/settings.py
@@ -192,7 +192,7 @@ if env('MODE') == 'Dev':
 if env('MODE') == 'Prod':
 
     # SECURE_SSL_REDIRECT = True  # This breaks things. 
-    CSRF_COOKIE_SECURE = True # This active may break the Django Admin.
+    # CSRF_COOKIE_SECURE = True # This active may break the Django Admin.
     SESSION_COOKIE_SECURE = True
     
     AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
