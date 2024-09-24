8eeb199749b9a9a7dabbc8a1cbd231b3018d8350
Tyler Stright <tylers@nezperce.org>
2024-09-24 11:30:31 -0600

    updated activity serializer depth for GET requests, functional.

--- cdms/serializers.py
+++ cdms/serializers.py
@@ -20,20 +20,20 @@ class InstrumentSerializer(MetaModelSerializer):
         fields = ['id', 'name', 'description', 'project', 'type', 'model', 'serial_number', 'manufacturer', 'is_active']
 
 class ActivitySerializer(MetaModelSerializer):
-    dataset = serializers.PrimaryKeyRelatedField(queryset=Dataset.objects.all())
-    project = serializers.PrimaryKeyRelatedField(queryset=Project.objects.all())
-
     class Meta:
         model = Activity
         fields = ['id', 'project', 'dataset', 'date', 'data'] # location, instrument
+        depth = 0
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Check if 'request' exists in context and is a GET request
+        request = self.context.get('request', None)
+        if request and request.method == 'GET':
+            self.Meta.depth = 2  # Set depth to 2 for GET requests
+        else:
+            self.Meta.depth = 0  # Set depth to 1 for all others
 
-    def to_representation(self, instance):
-        # Override field representation for GET requests
-        ret = super().to_representation(instance)
-        if self.context['request'].method == 'GET':
-            ret['dataset'] = DatasetSerializer(instance.dataset).data
-            ret['project'] = ProjectSerializer(instance.project).data
-        return ret
 
 class FieldSerializer(MetaModelSerializer):
     class Meta:
