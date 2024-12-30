4f9c91b28f1eaeb829c9f3674d1a08eecc28f314
Tyler Stright <tylers@nezperce.org>
2024-12-30 11:53:10 -0700

    updated fields view to include activity fields along with provided task type

--- data/views.py
+++ data/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets, permissions
 from data.models import Activity, Instrument, Field
 from common.models import ObjectLookUp
 from data.serializers import ActivitySerializer, InstrumentSerializer, FieldSerializer
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, get_list_or_404
 
 class InstrumentViewSet(viewsets.ModelViewSet):
     queryset = Instrument.objects.all()
@@ -25,8 +25,12 @@ class FieldViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         task_type = self.request.query_params.get('task_type')
 
+        # always include activity fields
+        activity_field_task_types = ObjectLookUp.objects.filter(name="Activity Field").values_list('id', flat=True)
+
+
         if task_type:
-            task_type= get_object_or_404(ObjectLookUp, id=task_type)
-            return Field.objects.filter(task_type=task_type)
-        else:
-            return Field.objects.all()
+            combined_task_types = list(activity_field_task_types) + [task_type]
+            return Field.objects.filter(task_type__id__in=combined_task_types)
+        
+        return Field.objects.all()
