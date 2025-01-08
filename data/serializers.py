3337692c796a8a3341b21fc80e929c1e531633ae
Tyler Stright <tylers@nezperce.org>
2025-01-08 07:42:06 -0700

    commented data content out, mostly.  the reference in the instrument model to objectlookup was creating problems, despite not being migrated.

--- data/serializers.py
+++ data/serializers.py
@@ -1,32 +1,32 @@
-from common.serializers import MetaModelSerializer
-from rest_framework import serializers
-from data.models import Instrument, Field, Activity
+# from common.serializers import MetaModelSerializer
+# from rest_framework import serializers
+# from data.models import Instrument, Field, Activity
 
-class InstrumentSerializer(MetaModelSerializer):
-    class Meta:
-        model = Instrument
-        fields = ['id', 'name', 'description', 'instrument_type', 'model', 'serial_number', 'manufacturer', 'display_name', 'is_active']
+# class InstrumentSerializer(MetaModelSerializer):
+#     class Meta:
+#         model = Instrument
+#         fields = ['id', 'name', 'description', 'instrument_type', 'model', 'serial_number', 'manufacturer', 'display_name', 'is_active']
 
-class ActivitySerializer(MetaModelSerializer):
-    class Meta:
-        model = Activity
-        fields = ['id', 'activity_id', 'location', 'instrument', 'task', 'header', 'detail', 'updated_at']
-        depth = 0
+# class ActivitySerializer(MetaModelSerializer):
+#     class Meta:
+#         model = Activity
+#         fields = ['id', 'activity_id', 'location', 'instrument', 'task', 'header', 'detail', 'updated_at']
+#         depth = 0
     
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        # Check if 'request' exists in context and is a GET request
-        request = self.context.get('request', None)
-        if request and request.method == 'GET':
-            self.Meta.depth = 2  # Set depth to 2 for GET requests
-        else:
-            self.Meta.depth = 0  # Set depth to 1 for all others
+#     def __init__(self, *args, **kwargs):
+#         super().__init__(*args, **kwargs)
+#         # Check if 'request' exists in context and is a GET request
+#         request = self.context.get('request', None)
+#         if request and request.method == 'GET':
+#             self.Meta.depth = 2  # Set depth to 2 for GET requests
+#         else:
+#             self.Meta.depth = 0  # Set depth to 1 for all others
 
 
-class FieldSerializer(MetaModelSerializer):
-    class Meta:
-        model = Field
-        fields = [
-            'id', 'task_type', 'field_for', 'sortingOrder', 'required', 'headerName', 'field', 'filter', 'editable', 
-            'pinned', 'width', 'minWidth', 'maxWidth', 'hide', 'cellRenderer', 'cellStyle', 'cellClass', 
-            'valueFormatter', 'headerTooltip', 'cellEditor', 'cellEditorParams']
+# class FieldSerializer(MetaModelSerializer):
+#     class Meta:
+#         model = Field
+#         fields = [
+#             'id', 'task_type', 'field_for', 'sortingOrder', 'required', 'headerName', 'field', 'filter', 'editable', 
+#             'pinned', 'width', 'minWidth', 'maxWidth', 'hide', 'cellRenderer', 'cellStyle', 'cellClass', 
+#             'valueFormatter', 'headerTooltip', 'cellEditor', 'cellEditorParams']
--- data/views.py
+++ data/views.py
@@ -1,32 +1,32 @@
-from rest_framework import viewsets, permissions
-from data.models import Activity, Instrument, Field
-from common.models import ObjectLookUp
-from data.serializers import ActivitySerializer, InstrumentSerializer, FieldSerializer
-from django.shortcuts import get_object_or_404
+# from rest_framework import viewsets, permissions
+# from data.models import Activity, Instrument, Field
+# from common.models import ObjectLookUp
+# from data.serializers import ActivitySerializer, InstrumentSerializer, FieldSerializer
+# from django.shortcuts import get_object_or_404
 
-class InstrumentViewSet(viewsets.ModelViewSet):
-    queryset = Instrument.objects.all()
-    serializer_class = InstrumentSerializer
-    lookup_field = 'name'
-    permission_classes = [permissions.DjangoModelPermissionsOrAnonReadOnly]
+# class InstrumentViewSet(viewsets.ModelViewSet):
+#     queryset = Instrument.objects.all()
+#     serializer_class = InstrumentSerializer
+#     lookup_field = 'name'
+#     permission_classes = [permissions.DjangoModelPermissionsOrAnonReadOnly]
 
-class ActivityViewSet(viewsets.ModelViewSet):
-    queryset = Activity.objects.all()
-    serializer_class = ActivitySerializer
-    lookup_field = 'id'
-    permission_classes = [permissions.DjangoModelPermissionsOrAnonReadOnly]
+# class ActivityViewSet(viewsets.ModelViewSet):
+#     queryset = Activity.objects.all()
+#     serializer_class = ActivitySerializer
+#     lookup_field = 'id'
+#     permission_classes = [permissions.DjangoModelPermissionsOrAnonReadOnly]
 
-class FieldViewSet(viewsets.ModelViewSet):
-    queryset = Field.objects.all()
-    serializer_class = FieldSerializer
-    lookup_field = 'id'
-    permission_classes = [permissions.DjangoModelPermissionsOrAnonReadOnly]
+# class FieldViewSet(viewsets.ModelViewSet):
+#     queryset = Field.objects.all()
+#     serializer_class = FieldSerializer
+#     lookup_field = 'id'
+#     permission_classes = [permissions.DjangoModelPermissionsOrAnonReadOnly]
 
-    def get_queryset(self):
-        task_type = self.request.query_params.get('task_type')
+#     def get_queryset(self):
+#         task_type = self.request.query_params.get('task_type')
 
-        if task_type:
-            task_type= get_object_or_404(ObjectLookUp, id=task_type)
-            return Field.objects.filter(task_type=task_type)
-        else:
-            return Field.objects.all()
+#         if task_type:
+#             task_type= get_object_or_404(ObjectLookUp, id=task_type)
+#             return Field.objects.filter(task_type=task_type)
+#         else:
+#             return Field.objects.all()
